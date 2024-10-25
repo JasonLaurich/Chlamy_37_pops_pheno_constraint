f63d305c6d79c95c5a017886d014b24ca90dbbc9
Jason Laurich <jlaurich@uoguelph.ca>
2024-10-25 09:10:45 -0400

    Updated 01_r_tpc_fitting
    Uploaded, transformed data

--- R-scripts/01_r_tpc_fitting.R
+++ R-scripts/01_r_tpc_fitting.R
@@ -8,11 +8,22 @@
 
 ######################### Upload packages ###################################
 
-
+library(cowplot)
+library(tidyverse)
 
 ######################### Upload and transform data #########################
 
+df <- read.csv("data-processed/chlamee-acute-rfu-time.csv")
+head(df) #RFU is density, days is time, temperature is temperature
+str(df)
 
+df$tfac <- as.factor(df$temperature)
+df$pop <- as.factor(df$population)
+
+str(df) # 38 populations?
+levels(df$pop) # I don't recognize the cc1629 group, but we'll keep it for now
+
+df$logRFU <- log(df$RFU) # Let's take the natural logarithm of RFU so we can fit an easy logarithmic growth curve.
 
 ######################### Estimate maximum growth rate, r ###################
 
