0ee4c485ea38874a045ea05da67ae802c4a7fd55
Jason Laurich <jlaurich@uoguelph.ca>
2024-11-12 09:06:45 -0500

    Created 03_TPC_fitting
    A script to fit TPCs to all populations using both nls and bayesian approaches.

---
+++ R-scripts/03_TPC_fitting.R
@@ -0,0 +1,18 @@
+# Jason R Laurich
+# November 12, 2024
+
+# This script will upload previously tabulated measurements of r for our 37 populations of Chlamydomonas at
+# various temperatures, and fit TPCs to the data, using both Bayesian and nls methods.
+
+############# Packages ########################
+
+
+############# Upload and examine data #######################
+
+
+############# Explore fitting TPCs ###############################
+
+############# Loop through entire dataset ################
+
+
+############# Figure generation and file export ########################
