5bc82f077341fec89d3ba3be1c5eef81b8165d50
Jason Laurich <jlaurich@uoguelph.ca>
2025-01-14 13:12:30 -0500

    Updated 06_light_monod.R
    Got the looping to correctly estimate r across all wells

--- R-scripts/06_light_monod.R
+++ R-scripts/06_light_monod.R
@@ -11,6 +11,7 @@
 
 ############# Packages ########################
 
+library(nls.multstart)
 library(tidyr)
 library(cowplot)
 library(ggplot2)
@@ -51,11 +52,102 @@ df.exp <- df.exp %>% # Recombine this with our dataframe
 
 mat.exp <- split(df.exp, df.exp$pop.num)  # Each element is a data frame for one population in df.exp
 
-############# Data exploration #####################
-
-
-
 ############# Loop through all populations ###################
+
+df.r.exp <- data.frame( # Initializing a dataframe to store the results for each well, pop, and light level
+  population = character(),
+  population.number = numeric(),
+  light.lvl = numeric(),
+  well.ID = character(),
+  r.exp = numeric()
+)
+
+light <- as.vector(as.numeric(as.character(unique(df.exp$percentage)))) # for looping through light levels
+ord.light<- sort(light)
+
+for (i in 1:length(mat.exp)){ # Looping through all of the populations
+  
+  for (t in ord.light){ # and all of the light levels
+    
+    df.it <- subset(mat.exp[[i]], percentage==t)
+    df.it <- droplevels(df.it) # Drop unused levels to isolate well replicate IDs at given t
+    
+    for (w in 1:length(levels(df.it$well.ID))){
+      
+      df.it.wl <- subset(df.it, as.numeric(df.it$well.ID) == w)
+      
+      if (df.it.wl$RFU[2] <  df.it.wl$RFU[1]){ # Get rid of 1st data points where there is a drop off after the first observation
+        df.it.wl <- df.it.wl[-1,]
+        df.it.wl$N0 <- df.it.wl$RFU[1]
+      }
+      
+      t.series <- unique(df.it.wl$days) # Re-initialize this internally - we will only save summary data for each unique pop x T x well combo
+      t.series <- t.series[-1] # Trim off the first entry to make tracking easier.
+      
+      ln.slopes <- c() # Re-initialize this too!
+      
+      for (z in t.series){
+        
+        df.it.wl.sl <- df.it.wl[df.it.wl$days <= z, ] # Subset the data to exclude time points above our window
+        
+        ln_slope <- lm(logRFU~days, data = df.it.wl.sl)
+        
+        ln.slopes <- c(ln.slopes, summary(ln_slope)$coefficients[2,1])
+        
+      } # So now we have our slopes for each well.ID x Pop x T level
+      
+      s <- length(ln.slopes) # Initialize the full length of the dataset, for cases in which the entire period is exponential growth.
+      
+      for (r in 1:(length(ln.slopes) - 1)) { # Loop through slopes to find when the drop-off exceeds 5%
+        
+        if (ln.slopes[r] != 0 & round(ln.slopes[r], digits = 5) !=0) { # We also need to account for tiny values that are basically 0 (e.g. 5 e-16, but are messing up our loops)
+          # percent.chg <- (ln.slopes[s] - ln.slopes[s + 1]) / ln.slopes[s] This was the reason we were getting weird negative results! If there was a stochastic drop.
+          percent.chg <- ln.slopes[r] / ln.slopes[r + 1] # This should (along with the next line) fix it.
+          
+          if (percent.chg >= 1.05 & ln.slopes[r] > 0 & ln.slopes[r+1] > 0) { # Because now, the drop-off ignores transiently negative slopes. 
+            s <- r # If the condition is met, reassign s to the corresponding drop-off point!
+            break  # Exit loop when condition is met
+          }
+        }
+      } # Now I have s!
+      
+      df.it.wl.th <- df.it.wl[df.it.wl$days <= t.series[s], ] # Get the thresholded data according to our sliding window approach
+      
+      r_exp <- nls_multstart(RFU ~ N0 * exp(r*days),  # Exponential growth model (N0 is in our dataframe)
+                             data = df.it.wl.th,
+                             start_lower = c(r = -4.5), 
+                             start_upper = c(r = 4.5),   
+                             iter = 500,
+                             supp_errors = 'Y',
+                             control = nls.control(maxiter = 200))
+      
+      if (is.null(r_exp)){
+        
+        df.r.exp <- rbind(df.r.exp, data.frame(
+          population = df.it.wl.th$pop.fac[1],          
+          population.number = df.it.wl$pop.num[1],      
+          temperature = df.it.wl$temperature[1],        
+          well.ID = df.it.wl$well.ID[1],                
+          r.exp = NA        
+        ))
+        
+      }else{
+        # Add data to our summary table
+        df.r.exp <- rbind(df.r.exp, data.frame(
+          population = df.it.wl$pop.fac[1],          # Population as factor
+          population.number = df.it.wl$pop.num[1],      # Numeric population number
+          light.lvl = df.it.wl$light_level[1],        # Temperature
+          well.ID = df.it.wl$well.ID[1],                # Well ID (assuming one well ID per subset)
+          r.exp = summary(r_exp)$parameters[1,1]        # The calculated r.exp value
+        ))
+        
+      }
+      
+    }
+    
+  }
+  
+}
 
 
 ############# Fit Monod curves to data ###################
