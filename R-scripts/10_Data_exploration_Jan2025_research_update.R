19921da52938e023778e048789fc898aa1ed3aaf
Jason Laurich <jlaurich@uoguelph.ca>
2025-01-21 09:22:41 -0500

    Created 10_data_exploration....
    Script for analyzing data for a Jan 21 research update in lab meeting and generating figures therefore.

---
+++ R-scripts/10_Data_exploration_Jan2025_research_update.R
@@ -0,0 +1,14 @@
+# Jason R Laurich
+# January 21, 2025
+
+# OK so here I am going to load up all of my previously summarized datasets.
+# I'll add in a few key checks (e.g. matching them up to original data to make sure my population labels are consistent across the board).
+# I'll also want to incorporate ancestry and evolutionary environment as factors.
+# And I'll want to calculate 1/R* (competitive ability) for N, P, and I.\
+
+# Then we'll do some PCAs (maybe accounting for ancestry? Evolutionary environment?) and generate some simple plots showcasing differences in TPC, R* shape.
+
+############# Packages ########################
+
+
+############# Upload and examine data #######################
