79227b67e9511658a28f627c60f12b5e8abb4fb9
Jason Laurich <jlaurich@uoguelph.ca>
2025-03-05 15:37:20 -0500

    Updated 15_Thomas...R
    Troubleshooting early issues with R2jags (due to the sheer breadth of T's) and implemented a nls.multstart / rTPC approach for comparison

--- R-scripts/15_Thomas_2012_TPC_fitting.R
+++ R-scripts/15_Thomas_2012_TPC_fitting.R
@@ -12,6 +12,8 @@ library(R2jags) # Fits Bayesian models
 library(mcmcplots) # Diagnostic plots for fits
 library(gridExtra)
 library(Deriv)
+library(rTPC)
+library(nls.multstart)
 
 ############# Upload and organize data #######################
 
@@ -39,9 +41,9 @@ df.i <- subset(mat[[i]])
 
 inits.lactin.thomas<- function() { # The final initial values set we landed on after experimenting. 
   list(
-    cf.a = runif(1, 0.05, 0.15),  # More constrained initial values
+    cf.a = runif(1, 0.05, 1),  # More constrained initial values
     cf.tmax = runif(1, 1, 40),    # Much wider to accomodate interspecific variation
-    cf.delta_t = runif(1, 1, 5),
+    cf.delta_t = runif(1, 1, 10),
     cf.b = runif(1, -2.5, -1),
     cf.sigma = runif(1, 0.1, 2)
   )
@@ -51,7 +53,6 @@ ni.fit <- 33000    # iterations / chain
 nb.fit <- 3000     # burn in periods for each chain
 nt.fit <- 30       # thinning interval : (33,000 - 3,000) / 30 = 1000 posterior estimates / chain
 nc.fit <- 3        # number of chains, total of 3,000 estimates for each model. 
-
 
 parameters.lactin2 <- c("cf.a", "cf.b", "cf.tmax", "cf.delta_t", "cf.sigma", "r.pred") # repeated here
 
@@ -75,6 +76,52 @@ lac_jag <- jags(
   n.iter = ni.fit, 
   DIC = TRUE, 
   working.directory = getwd()
+) # ~ 1 min to run...
+
+lac_jag$BUGSoutput$summary[1:6,] # terrible with the expanded cf.a and delta_t
+mcmcplot(lac_jag) # I can't live with that for sure
+
+df.jags <- data.frame(lac_jag$BUGSoutput$summary)
+df.jags.plot <- df.jags[-c(1:6),]
+df.jags.plot$temp <- seq(-5, 45, 0.1)
+
+lact.jag.plot <- ggplot(data = df.jags.plot, aes(x = temp)) +
+  geom_ribbon(aes(ymin = X2.5., ymax = X97.5.), fill = "gold", alpha = 0.5) +# Add shaded uncertainty region (LCL to UCL)
+  geom_line(aes(y = mean), color = "darkorchid", size = 1) + # Add the mean prediction line
+  geom_point(data = df.i, aes(x = jitter(Temperature, 0.5), y = Growth.rate), color = "grey9", size = 2) + # Add observed data points with jitter for Temp
+  scale_x_continuous(limits = c(-5, 45)) + 
+  scale_y_continuous(limits = c(-5, 5)) + # Customize the axes and labels +
+  labs(
+    x = expression(paste("Temperature (", degree, "C)")),
+    y = "Growth rate",
+    title = "Lactin 2 Model Fit, Constrained, Doubled"
+  ) +
+  theme_classic() +
+  geom_hline(yintercept = 0)
+
+lact.jag.plot
+
+# Let's try a frequentist approach
+
+start.vals.lac <- get_start_vals(df.i$Temperature, df.i$Growth.rate, model_name = 'lactin2_1995')
+
+lac_nls <- nls_multstart(Growth.rate ~ lactin2_1995(temp = Temperature, a, b, tmax, delta_t),
+                         data = df.i,
+                         iter = c(4, 4, 4, 4), 
+                         start_lower = start.vals.lac - 10,
+                         start_upper = start.vals.lac + 10,
+                         lower = get_lower_lims(df.i$Temperature, df.i$Growth.rate, model_name = 'lactin2_1995'),
+                         upper = get_upper_lims(df.i$Temperature, df.i$Growth.rate, model_name = 'lactin2_1995'),
+                         supp_errors = 'Y',
+                         convergence_count = FALSE
 )
+
+summary(lac_nls)
+
+preds.lac <- data.frame(Temperature = seq(min(df.i$Temperature - 2), max(df.i$Temperature +2), length.out = 100))
+preds.lac <- broom::augment(lac_nls, newdata = preds.lac)
+
+lac_plot <- ggplot(preds.lac) + geom_point(aes(Temperature, Growth.rate), df.i) +
+  geom_line(aes(Temperature, .fitted), col = 'darkslateblue') + theme_classic() +ggtitle('Lactin 2') +ylim(-3,5) # This is substantially better. Why?
 
 ############# Fit TPCs and save the summary statistics #######################
