9761bcbec8037382f9c81d2a0fc9dc6bbd9e1120
Jason Laurich <jlaurich@uoguelph.ca>
2025-03-11 11:39:48 -0400

    Updated 15_Thomas...R
    Done up to 145/194

--- R-scripts/15_Thomas_2012_TPC_fitting.R
+++ R-scripts/15_Thomas_2012_TPC_fitting.R
@@ -341,7 +341,7 @@ nc.fit <- 3         # number of chains, total of 3,000 estimates for each model.
 thomas.summ.df <- read.csv("data-processed/17_Thomas2012_TPCs.csv") # TPC parameters
 fit.df <- read.csv("data-processed/17a_Thomas2012_TPCs_fits.csv")   # Model fit details
 
-for (i in 128:194){
+for (i in 146:194){
   
   df.i <- subset(mat[[i]])
   
