bd7999250de4f037c748bbe07ca2739b29156155
Jason Laurich <jlaurich@uoguelph.ca>
2025-04-13 16:21:45 -0400

    Added 18_narwani...R
    Booted up a script to process summary data from Narwani et al 2015 (plos one). N, P and light data for 51 species

---
+++ R-scripts/18_Narwani_2015_model_fitting.R
@@ -0,0 +1,24 @@
+# Jason R Laurich
+# April 13, 2025
+
+# Going to work with the Narwani et al 2015 (https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0137085#references) data to fit
+# Monod curves for nitrogen,light, and phosphorous. No temperature data. 
+
+
+# Load packages -----------------------------------------------------------
+
+
+
+# Load and examine data ---------------------------------------------------
+
+
+
+# Nitrogen Monod ----------------------------------------------------------
+
+
+# Phosphorous Monod -------------------------------------------------------
+
+
+# Light Monod -------------------------------------------------------------
+
+
