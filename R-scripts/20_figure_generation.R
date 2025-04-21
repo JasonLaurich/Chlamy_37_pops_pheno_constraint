5c7e2f721124f122585aa6cca1e1a387c98d03c0
Jason Laurich <jlaurich@uoguelph.ca>
2025-04-21 13:10:34 -0400

    Updated 20_figure_generation.R, added 24a_stoich_NP.csv
    Added csv with N, P stoichiometry data (I'm unsure about using the C/N data due to population #ing issues), working through the beginning of figure generation.

--- R-scripts/20_figure_generation.R
+++ R-scripts/20_figure_generation.R
@@ -13,6 +13,60 @@ library(gridExtra)
 
 # Load and examine data ---------------------------------------------------
 
+df<-read.csv("data-processed/14_summary_metric_table.csv") # Summary file
+
+head(df)
+str(df)
+
+levels(as.factor(df$Pop.fac)) # Character-coded, for ancestral populations as well. 
+
+df.stoich <- read.csv("data-processed/24_stoich_data.csv") # Stoichiometry data
+
+head(df.stoich)
+str(df.stoich)
+
+levels(as.factor(df.stoich$Name)) # These are 1 - 40 (ie I need to match them to their corresponding population names)
+
+df.pig <- read.csv("data-processed/23a_pigment_data.csv") # Pigment data
+
+head(df.pig)
+str(df.pig)
+
+levels(as.factor(df.pig$HPLC.Nummer)) # Hmm so these are already numbered 1-37, ie. the missing populations do not feature here. 
+
+length(unique(df.stoich$Name))
+length(unique(df.pig$HPLC.Nummer))
+length(unique(df$Pop.fac))
+
+# OK so the following #s should be missing: 12, 13, 34, anc1 (35 + 5 ancestors + 1 general anc = 41 total)
+# Missing 4 makes the math work!
+
+levels(as.factor(df.stoich$Name))
+length(unique(df.stoich$Name)) # Weirdly I have stoichiometry data for 12 and 13, but not 27 and 29? I also have it for 34?
+# So we have three extra reads, but are missing 2 — 38 makes sense
+# I don't know how to make sense of this I will talk to Joey about this.
+
+levels(as.factor(df.pig$HPLC.Nummer)) # Ok so we can maybe just assume these are ordered?
+
+df.stoich.NP <- read.csv("data-processed/24a_stoich_NP.csv") # Stoichiometry data for N and P
+
+levels(as.factor(df.stoich.NP$Name)) # Ok so we can maybe just assume these are ordered?
+length(unique(df.stoich.NP$Name)) # OK that matches better
+
+df.id <- read.csv("data-processed/22_stoich_pigment_ID_mapping.csv") # File containing the identity assignment
+
+df.id
+
+df.stoich.NP %>% 
+  group_by(Name) %>% 
+  summarize(mean.N.µg.l = mean(N.µg.l), mean.P.µg.l = mean(P.µg.l)) %>% 
+  print(n=37)
+
+df.stoich.NP.sum <- df.stoich.NP %>% 
+  group_by(Name) %>% 
+  summarize(mean.N.µg.l = mean(N.µg.l), mean.P.µg.l = mean(P.µg.l)) %>% 
+  print(n=37)
+
 
 
 # Figure 1: PCAs and RDAs -----------------------------------------------------------
