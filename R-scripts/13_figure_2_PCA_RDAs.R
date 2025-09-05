2c232edfd68c885ff33d3a42fb0e2f5252f30bd0
Jason Laurich <jlaurich@uoguelph.ca>
2025-09-05 19:48:15 -0400

    Updated 13_figure...R
    Finished adjusting label placement for the PCA plot (fig 2). RDAs (supplements) up next

---
+++ R-scripts/13_figure_2_PCA_RDAs.R
@@ -0,0 +1,148 @@
+# Jason R Laurich
+
+# September 5th, 2025
+
+# Creating Figure 2: PCA and RDA analyses (RDA to be included in the supplement)
+
+# Packages & Functions ----------------------------------------------------
+
+library(tidyverse)
+library(cowplot)
+library(vegan)  # For PCA and RDA
+
+# Load & examine the data -------------------------------------------------
+
+df <- read.csv("data-processed/20_summary_table.csv") # Summary file
+str(df)
+head(df)
+
+df$Evol.plt <- factor(df$Evol, 
+                      levels = c("none", "L", "N", "P", "S", "B", "BS", "C"),
+                      labels = c("Ancestral",
+                                 "Light limitation",
+                                 "Nitrogen limitation", 
+                                 "Phosphorous limitation", 
+                                 "Salt stress", 
+                                 "Biotic depletion", 
+                                 "Biotic depletion x Salt", 
+                                 "Control"))
+
+df$Anc.plt <- factor(df$Anc, 
+                     levels = c("anc2", "anc3", "anc4", "anc5", "cc1690"),
+                     labels = c("Population 2", 
+                                "Population 3", 
+                                "Population 4", 
+                                "Population 5", 
+                                "Mixed population"))
+
+df.pca <- df %>% select(T.br.0.56, T.µ.max, I.comp.0.56, I.µ.max, N.comp.0.56, N.µ.max, P.comp.0.56, P.µ.max, S.µ.max, 
+                              S.c, chl.a, chl.b, luthein, mean.N.µg.l, mean.P.µg.l, Evol.plt) # Prepare the data: selecting only the relevant columns
+df.pca
+
+df.pca <- df.pca[-31,] # For now removing wonky/missing points (I.comp is way too high in row 31)
+
+evol.pca <- df.pca$Evol.plt
+
+df.pca <- df.pca %>% select(-Evol.plt)
+
+pca.result <- prcomp(df.pca, center = TRUE, scale. = TRUE) # Perform PCA
+pca.result
+
+sdev <- pca.result$sdev  
+
+sdev %>%
+  { .^2 / sum(.^2) } %>%
+  print() 
+
+df.pca.res <- data.frame(pca.result$x, evol.pca)  # Add grouping factor
+colnames(df.pca.res) <- c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", 
+                          "PC10", "PC11", "PC12", "PC13", "PC14", "PC15", "Evolution")
+
+PCA <- ggplot(df.pca.res, aes(x = PC1, y = PC2, color = evol.pca)) +  # PCA biplot visualization
+  geom_point(size = 3) +
+  theme_classic() +
+  labs(x = paste("PC1 (", round(pca.result$sdev[1]^2 / sum(pca.result$sdev^2) * 100, 2), "%)", sep = ""),
+       y = paste("PC2 (", round(pca.result$sdev[2]^2 / sum(pca.result$sdev^2) * 100, 2), "%)", sep = ""),
+       title = "PCA of Thermal Performance and Competitive Abilities") +
+  scale_color_manual(
+    name = "Evolution environment",  # Update the legend title
+    values = c("Biotic depletion" = "darkorange",
+               "Biotic depletion x Salt" = "deepskyblue1",
+               "Control" = "forestgreen",
+               "Light limitation" = "gold",
+               "Nitrogen limitation" = "magenta3",
+               "Ancestral" = "black",
+               "Phosphorous limitation" = "firebrick",  
+               "Salt stress" = "blue")
+  )
+
+PCA
+
+loadings <- as.data.frame(pca.result$rotation) # Extract PCA loadings (rotation matrix)
+
+loadings$PC1 <- loadings$PC1 * max(abs(df.pca.res$PC1)) # Scale loadings to fit within the PCA plot (can adjust scaling factor)
+loadings$PC2 <- loadings$PC2 * max(abs(df.pca.res$PC2))
+
+loadings$variable <- rownames(loadings) # Add variable names for annotation
+
+loadings$metric <- factor(loadings$variable, 
+                          levels = c("T.br.0.56", "T.µ.max","I.comp.0.56", "I.µ.max", "N.comp.0.56", "N.µ.max",
+                                     "P.comp.0.56", "P.µ.max", "S.µ.max", "S.c", "chl.a", "chl.b", "luthein",
+                                     "mean.N.µg.l", "mean.P.µg.l"),
+                          labels = c("Thermal~breadth",
+                                     "mu~max~(T)", 
+                                     "1/I^\"*\"", 
+                                     "mu~max~(I)",
+                                     "1/N^\"*\"", 
+                                     "mu~max~(N)",
+                                     "1/P^\"*\"", 
+                                     "mu~max~(P)", 
+                                     "mu~max~(Salt)", 
+                                     "Salt~tolerance",
+                                     "Chlorophyll~italic(a)",
+                                     "Chlorophyll~italic(b)",
+                                     "Luthein", 
+                                     "N~content",
+                                     "P~content"))
+
+adjust.x <- c(1.2, 0.4, 0.05, 0.67, 0.25, 0.75, 0.25, -0.05, -0.05, 0.8, 0.95, 0.45, 0.55, 0.67, 0.67) # adjustments for each label
+adjust.y <- c(0.05, 0, -0.02, 0.11, -0.01, 0.15, -0.01, 0.1, 0.1, -0.02, 0.05, 0.2, -0.05, -0.05, -0.05)
+
+loadings$var.x <- loadings$PC1 + adjust.x # Need to manually space out labels here. 
+loadings$var.y <- loadings$PC2 + adjust.y
+
+# Create PCA plot with arrows
+pca_plot_arrows <- ggplot(df.pca.res, aes(x = PC1, y = PC2, color = Evolution)) +
+  geom_point(size = 3) +
+  theme_classic() +
+  labs(x = paste("PC 1 (", round(pca.result$sdev[1]^2 / sum(pca.result$sdev^2) * 100, 2), "%)", sep = ""),
+       y = paste("PC 2 (", round(pca.result$sdev[2]^2 / sum(pca.result$sdev^2) * 100, 2), "%)", sep = "")) +
+  scale_color_manual(
+    name = "Evolution environment",  # Update the legend title
+    values = c("Biotic depletion" = "darkorange",
+               "Biotic depletion x Salt" = "deepskyblue1",
+               "Control" = "forestgreen",
+               "Light limitation" = "gold",
+               "Nitrogen limitation" = "magenta3",
+               "Ancestral" = "black",
+               "Phosphorous limitation" = "firebrick",  
+               "Salt stress" = "blue")
+  ) +  # Use your custom colors
+  # Add arrows for variable contributions
+  geom_segment(data = loadings, aes(x = 0, y = 0, xend = PC1, yend = PC2),
+               arrow = arrow(length = unit(0.2, "cm")), color = "black", size= 1.1) +
+  scale_x_continuous(breaks = seq(-5, 5, by = 1)) +
+  scale_y_continuous(breaks = seq(-5, 7, by = 1)) +
+  # Add variable names to the plot
+  geom_text(data = loadings, aes(x = var.x, y = var.y, label = metric),
+            vjust = 1, hjust = 1,  color = "black", size = 5, parse = T) +
+  theme(legend.position= c(0.25, 0.25),
+        legend.title = element_text(size = 12, face = "bold"),
+        legend.text  = element_text(size = 12, face = "plain"),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.title.y = element_text(size = 12, face = "bold")) 
+
+pca_plot_arrows
+
+ggsave("figures/09_fig2_PCA.jpeg", pca_plot_arrows, width = 12, height = 9)
+
