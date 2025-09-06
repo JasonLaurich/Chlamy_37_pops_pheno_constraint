4e6af13ec263a167ae82214224fbdb5a7ab9d3e5
Jason Laurich <jlaurich@uoguelph.ca>
2025-09-06 13:21:39 -0400

    Created 14_figure_3_intra_gradient_tradeoffs.R
    A script to examine Pareto front optimatlity and tradeoffs within gradients for our 37 populations of C. reinhardtii. Will also feature statistical testing related to these figures.

---
+++ R-scripts/14_figure_3_intra_gradient_tradeoffs.R
@@ -0,0 +1,13 @@
+# Jason R Laurich
+
+# September 6th, 2025
+
+# Creating Figure 3: Intra-gradient Pareto front panels (light, nitrogen, phosphorous, salt, and temperature)
+# Am also going to include relevant statistical testing here. 
+
+# Packages & Functions ----------------------------------------------------
+
+library(tidyverse)
+library(cowplot)
+
+# Load & examine the data -------------------------------------------------
