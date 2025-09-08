051495e4a7953903d625effa8b3267538de5eb61
Jason Laurich <jlaurich@uoguelph.ca>
2025-09-08 06:49:18 -0400

    Created 15_figure4...R
    Script to create figure 4 (interspecific trade-offs). Going to ignore this for now and circle back later

---
+++ R-scripts/15_figure_4_inter-specific_tradeoffs.R
@@ -0,0 +1,8 @@
+# Jason R Laurich
+
+# September 8th, 2025
+
+# Creating Figure 4: Intra-gradient Pareto front panels (light, nitrogen, phosphorous, and temperature) for multi-species datasets
+# Am also going to include relevant statistical testing here. 
+
+# Packages & Functions ----------------------------------------------------
