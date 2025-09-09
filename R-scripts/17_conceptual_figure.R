405a69acff987bb0e4007fc6b457a79101cb4136
Jason Laurich <jlaurich@uoguelph.ca>
2025-09-09 15:52:18 -0400

    Created 17_conceptual_figure.R
    A file for playing with and finalizing a conceptual figure depicting various kinds/ aspects of trade-offs and clarifying our approach and predictions

---
+++ R-scripts/17_conceptual_figure.R
@@ -0,0 +1,7 @@
+# Jason R Laurich
+
+# September 9th, 2025
+
+# Creating an introductory figure to lay out hypotheses and predictions/ serve as a roadmap for the paper
+
+# Packages & Functions ----------------------------------------------------
