3cfd1a8678b50ebfe00627aa4a9c92a2fa2aacff
Jason Laurich <jlaurich@uoguelph.ca>
2025-09-11 15:33:52 -0400

    Updated 17_conceptual...R
    Updating some thoughts about what this should look like as I read and think

--- R-scripts/17_conceptual_figure.R
+++ R-scripts/17_conceptual_figure.R
@@ -6,6 +6,15 @@
 
 # 3 panels?
 
+# Idea - working off of the Metcalfe 2025 JEB paper: one panel could be at the level of organism? The three ways in which trade-offs
+# can emerge as a result of energy ceilings? (3) plastic ceiling - location of PF could change within a single org.
+# How does this relate to our experiments? Physiological changes (epigenetic) or adaptive evolution of this frontier?
+
+# Idea - working of the Y-model resource-dependent trade off idea (e.g. Johnson and Nassrullah, 2025) - trade-offs could be detected 
+# with quantile regressions? What about at the bleeding edge (PFs)?!?
+# Also more variation in resource acquisition (e.g. 1/R*s) leads to weaker/ positive correlations? Could test this with my data
+# Both across the whole dataset and among Pareto-optimal points?
+
 # Packages & Functions ----------------------------------------------------
 
 library(dplyr)
