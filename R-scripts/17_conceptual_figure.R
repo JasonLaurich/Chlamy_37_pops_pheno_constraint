6259bc31134ce9ca5a357de8a1776634e3504364
Jason Laurich <jlaurich@uoguelph.ca>
2025-09-11 10:19:45 -0400

    Updated 7_conceptual...R

--- R-scripts/17_conceptual_figure.R
+++ R-scripts/17_conceptual_figure.R
@@ -4,6 +4,8 @@
 
 # Creating an introductory figure to lay out hypotheses and predictions/ serve as a roadmap for the paper
 
+# 3 panels?
+
 # Packages & Functions ----------------------------------------------------
 
 library(dplyr)
