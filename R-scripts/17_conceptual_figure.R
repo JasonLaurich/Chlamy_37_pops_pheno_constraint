8cc6f7866c19a3bee5cbec7b930225f2aecf4c76
Jason Laurich <jlaurich@uoguelph.ca>
2025-09-16 14:51:27 -0400

    Updated 17_concep...R
    Finished creating my inset panel

--- R-scripts/17_conceptual_figure.R
+++ R-scripts/17_conceptual_figure.R
@@ -27,5 +27,84 @@
 
 # Packages & Functions ----------------------------------------------------
 
-library(dplyr)
+library(tidyverse)
 library(cowplot)
+
+# Create panel A (inset) — variation w/in individuals  --------------------
+
+df.A2 <- data.frame(
+  x = c(3, 7),
+  y = c(23, 27)
+)
+
+poly.band <- data.frame(
+  x = c(2, 10, 5, 0),
+  y = c(20, 25, 30, 22)
+)
+
+p.A2 <- ggplot(df.A2, aes(x = x, y = y)) +
+  geom_point(size= 3) +
+  
+  scale_y_continuous(limits = c(20, 30), breaks = c(20, 25, 30)) +
+  scale_x_continuous(limits = c(0, 10), breaks = c(0, 5, 10)) +
+  
+  geom_segment(aes(x = 2, xend = 10, y = 20, yend = 25), colour = "black", size = 1.1) +
+  geom_segment(aes(x = 0, xend = 5, y = 22, yend = 30), colour = "black", size = 1.1) +
+  
+  geom_segment(
+    aes(x = 3.2, y = 23.2, xend = 6.8, yend = 26.8),
+    inherit.aes = FALSE,
+    colour = "red3", linewidth = 1.5,
+    arrow = arrow(length = unit(3, "mm"), type = "closed")
+  ) +
+  
+  annotate("curve",
+           x = 3.2,  y = 22.8,
+           xend = 3.8,   yend = 21.4,
+           curvature = -0.15, ncp = 50,
+           colour = "blue3", linewidth = 1.5,
+           arrow = arrow(length = unit(3, "mm"), type = "closed")
+  ) +
+  
+  annotate("curve",
+           x = 2.8,  y = 23.2,
+           xend = 1.4,   yend = 23.8,
+           curvature = 0.15, ncp = 50,
+           colour = "blue3", linewidth = 1.5,
+           arrow = arrow(length = unit(3, "mm"), type = "closed")
+  ) +
+  
+  annotate("curve",
+           x = 7.2,  y = 26.8,
+           xend = 8.3,   yend = 24.2,
+           curvature = -0.15, ncp = 50,
+           colour = "blue3", linewidth = 1.5,
+           arrow = arrow(length = unit(3, "mm"), type = "closed")
+  ) +
+  
+  annotate("curve",
+           x = 6.8,  y = 27.2,
+           xend = 4.4,   yend = 28.3,
+           curvature = 0.15, ncp = 50,
+           colour = "blue3", linewidth = 1.5,
+           arrow = arrow(length = unit(3, "mm"), type = "closed")
+  ) +
+  
+  geom_polygon(data = poly.band, aes(x, y),
+               fill = "grey80", alpha = 0.35, colour = NA,
+               inherit.aes = FALSE) +
+  
+  theme_classic() +
+  
+  labs(x = "Salt tolerance (c)",    
+       y = "Thermal breadth (°C)", 
+       title = "Inset — individual plasiticity") +  # labels
+  
+  theme(
+    legend.position = "none",  
+    axis.title = element_text(size = 12, face = "bold"),  
+    axis.text = element_text(size = 10, face ="plain"),
+    plot.title = element_text(size = 14, face = "bold", hjust = 0.03)# theme stuff
+  )
+
+p.A2
