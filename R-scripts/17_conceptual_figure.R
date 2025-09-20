a089ef839b446e4b4037e0f7b5ce677d28ce1e7e
Jason Laurich <jlaurich@uoguelph.ca>
2025-09-20 19:14:00 -0400

    Updated 17....R
    Working on an expanded and improved conceptual figure with 5 panels! More detail on how PFs should/could appear at various levels

--- R-scripts/17_conceptual_figure.R
+++ R-scripts/17_conceptual_figure.R
@@ -30,87 +30,14 @@
 library(tidyverse)
 library(cowplot)
 
-# Create panel A — variation across individuals/ genotypes ----------------
+# Create panel A — variation w/in individuals  --------------------
 
 df.A <- data.frame(
-  x = c(5.7, 6.2, 7.3, 7.4, 8.5, 7.8, 8.8, 9.1, 7.9, 9.5, 10.8, 10.1, 10.2, 11.2, 11.4, 11.3, 12.0, 13.9, 13.55, 13.1, 10.7, 14.55, 9.1, 12.5, 11.9, 14.45, 13.0),
-  y = c(5.8, 8.1, 6.5, 9.3, 6.3, 8.0, 12.0, 7.5, 10.6, 9.4, 8.2, 10.1, 12.9, 11.7, 7.4, 9.7, 13.6, 10.1, 12.3, 13.8, 14.55, 11.1, 14.1, 10.55, 14.45, 11.9, 8.7)
-)
-
-poly.band <- data.frame(
-  x = c(5, 7, 15, 9, 5),
-  y = c(5, 5, 9, 15, 7)
-)
-
-# Create a quadratic Bezier function to capture the curved pareto frontier
-
-M  <- c(13.5, 13.5)          # point the curve should pass through
-P0 <- c(15, 9)               # right endpoint
-P2 <- c(9, 15)               # top endpoint
-
-t0 <- 0.5
-P1 <- (M - (1 - t0)^2 * P0 - t0^2 * P2) / (2 * (1 - t0) * t0)  # (10,10)
-
-t <- seq(0, 1, length.out = 80)          # curve from P0 -> P2 (right -> top)
-curve_pts <- data.frame(
-  x = (1 - t)^2 * P0[1] + 2 * (1 - t) * t * P1[1] + t^2 * P2[1],
-  y = (1 - t)^2 * P0[2] + 2 * (1 - t) * t * P1[2] + t^2 * P2[2]
-)
-
-poly.crv <- rbind(
-  poly.band[1:3,],                         # (0,0)->(2,0)->(10,4)
-  curve_pts[-c(1, length(t)), ],           # curved edge (avoid duplicated ends)
-  poly.band[4:5,]                          # (4,10)->(0,2)
-)
-
-p.A <- ggplot(df.A, aes(x = x, y = y)) +
-  geom_point(size= 3) +
-  
-  scale_y_continuous(limits = c(5, 15), breaks = c(5, 10, 15)) +
-  scale_x_continuous(limits = c(5, 15), breaks = c(5, 10, 15)) +
-  
-  theme_classic() +
-  
-  geom_segment(
-    aes(x = 5.1, y = 5.1, xend = 13.4, yend = 13.4),
-    inherit.aes = FALSE,
-    colour = "red3", linewidth = 1.5) +
-  
-  annotate(
-    "rect",
-    xmin = 12, xmax = 14,
-    ymin = 7.7, ymax = 9.7,
-    fill = NA, colour = "black", linewidth = 1.5
-  ) +
-  
-  geom_polygon(data = poly.crv, aes(x, y),
-               fill = "grey60", alpha = 0.3, colour = NA,
-               inherit.aes = FALSE) +
-  
-  labs(x = "Trait 1 (e.g. maximum growth rate)",    
-       y = "Trait 2 (e.g. salt tolerance)", 
-       title = "A — variation among genotypes") +  # labels
-  
-  theme(
-    legend.position = "none",  
-    axis.title = element_text(size = 12, face = "bold"),  
-    axis.text = element_text(size = 10, face ="plain"),
-    plot.title = element_text(size = 14, face = "bold", hjust = 0.03)# theme stuff
-  ) +
-  
-  coord_cartesian(xlim = c(5,15), ylim = c(5,15), expand = FALSE)
-
-p.A
-
-
-# Create panel A (inset) — variation w/in individuals  --------------------
-
-df.A2 <- data.frame(
   y = c(8.7, 9.2),
   x = c(13, 13.5)
 )
 
-p.A2 <- ggplot(df.A2, aes(x = x, y = y)) +
+p.A <- ggplot(df.A, aes(x = x, y = y)) +
   geom_point(size= 3) +
   
   geom_polygon(data = poly.crv, aes(x, y),
@@ -121,11 +48,11 @@ p.A2 <- ggplot(df.A2, aes(x = x, y = y)) +
   geom_segment(aes(x = 12, xend = 13, y = 8.0, yend = 9.7), colour = "black", size = 1.1) +
   
   theme_classic() +
-
+  
   
   labs(x = "",    
        y = "", 
-       title = "Inset — individual plasiticity") +  # labels
+       title = "A — plastic variation") +  # labels
   
   theme(
     legend.position = "none",  
@@ -175,47 +102,195 @@ p.A2 <- ggplot(df.A2, aes(x = x, y = y)) +
   
   coord_cartesian(xlim = c(12,14), ylim = c(7.7,9.7), expand = FALSE)
 
-p.A2
+p.A
 
-p.top <- plot_grid(
-  p.A, p.A2,
-  ncol = 2,
-  rel_widths = c(1, 0.4),   # tweak this ratio to taste
-  rel_heights = c(1, 0.4),
-  align = ""              # align panels nicely
-)
-
-p.top
-
-p.A2.zoom <- p.A2 +
-  coord_equal(xlim = c(12, 14), ylim = c(7.7, 9.7), expand = FALSE) +
-  theme(text = element_text(size = 9),
-        axis.title = element_text(size = 10),
-        plot.margin = margin(4, 4, 4, 4))
-
-
-right_col <- plot_grid(
-  NULL,
-  p.A2.zoom,
-  NULL,
-  ncol = 1,
-  rel_heights = c(0.3, 1, 0.3)   # <- controls inset height in the column
-)
-
-right_col
-
-p.top <- plot_grid(
-  p.A, right_col,
-  ncol = 2,
-  rel_widths = c(1, 1),            # <- inset column ~40% of the width; tweak
-  align = "h"
-)
-
-p.top
-
-# Create panel B - interspecific variation ---------------------------------------
+# Create panel B — variation across individuals/ genotypes ----------------
 
 df.B <- data.frame(
+  x = c(5.7, 6.2, 7.3, 7.4, 8.5, 7.8, 8.8, 9.1, 7.9, 9.5, 10.8, 10.1, 10.2, 11.2, 11.4, 11.3, 12.0, 13.9, 13.55, 13.1, 10.7, 14.55, 9.1, 12.5, 11.9, 14.45, 13.0),
+  y = c(5.8, 8.1, 6.5, 9.3, 6.3, 8.0, 12.0, 7.5, 10.6, 9.4, 8.2, 10.1, 12.9, 11.7, 7.4, 9.7, 13.6, 10.1, 12.3, 13.8, 14.55, 11.1, 14.1, 10.55, 14.45, 11.9, 8.7)
+)
+
+poly.band <- data.frame(
+  x = c(5, 7, 15, 9, 5),
+  y = c(5, 5, 9, 15, 7)
+)
+
+# Create a quadratic Bezier function to capture the curved pareto frontier
+
+M  <- c(13.5, 13.5)          # point the curve should pass through
+P0 <- c(15, 9)               # right endpoint
+P2 <- c(9, 15)               # top endpoint
+
+t0 <- 0.5
+P1 <- (M - (1 - t0)^2 * P0 - t0^2 * P2) / (2 * (1 - t0) * t0)  # (10,10)
+
+t <- seq(0, 1, length.out = 80)          # curve from P0 -> P2 (right -> top)
+curve_pts <- data.frame(
+  x = (1 - t)^2 * P0[1] + 2 * (1 - t) * t * P1[1] + t^2 * P2[1],
+  y = (1 - t)^2 * P0[2] + 2 * (1 - t) * t * P1[2] + t^2 * P2[2]
+)
+
+poly.crv <- rbind(
+  poly.band[1:3,],                         # (0,0)->(2,0)->(10,4)
+  curve_pts[-c(1, length(t)), ],           # curved edge (avoid duplicated ends)
+  poly.band[4:5,]                          # (4,10)->(0,2)
+)
+
+p.B <- ggplot(df.B, aes(x = x, y = y)) +
+  
+  scale_y_continuous(limits = c(5, 16), breaks = c(5, 10, 15)) +
+  scale_x_continuous(limits = c(5, 16), breaks = c(5, 10, 15)) +
+  
+  theme_classic() +
+  
+  geom_segment(
+    aes(x = 5.1, y = 5.1, xend = 13.4, yend = 13.4),
+    inherit.aes = FALSE,
+    colour = "red3", linewidth = 1.5,
+    arrow = arrow(length = unit(2.1, "mm"), ends = "both", type = "closed")) +
+  
+  geom_line(data = curve_pts, aes(x = x, y = y), color = "orchid2", size = 1.5, inherit.aes = FALSE) +  # Pareto front
+  
+  annotate(
+    "rect",
+    xmin = 12, xmax = 14,
+    ymin = 7.7, ymax = 9.7,
+    fill = NA, colour = "black", linewidth = 1.5
+  ) +
+  
+  geom_polygon(data = poly.crv, aes(x, y),
+               fill = "grey60", alpha = 0.3, colour = NA,
+               inherit.aes = FALSE) +
+  
+  geom_point(size= 3) +
+  
+  labs(x = "Trait 1 (e.g. maximum growth rate)",    
+       y = "Trait 2 (e.g. salt tolerance)", 
+       title = "B — genotypic variation") +  # labels
+  
+  theme(
+    legend.position = "none",  
+    axis.title = element_text(size = 12, face = "bold"),  
+    axis.text = element_text(size = 10, face ="plain"),
+    plot.title = element_text(size = 14, face = "bold", hjust = 0.03)# theme stuff
+  ) +
+  
+  annotate("text", x = 13.8, y = 13.8, label = "Pareto front", size = 5.1, fontface = "bold", colour = "orchid2", angle = -45) +
+  
+  coord_cartesian(xlim = c(5,16), ylim = c(5,16), expand = FALSE) 
+
+p.B
+
+# Create panel C — variation across individuals/ genotypes with a gap ----------------
+
+df.C <- data.frame(
+  x = c(5.7, 6.2, 7.3, 7.4, 8.5, 7.8, 9.1, 7.90, 9.5, 10.8, 12.0, 14.1, 13.55, 13.1, 10.70, 14.55, 9.10, 11.90, 14.45, 10.1, 14.7, 13.1),
+  y = c(5.8, 8.1, 6.5, 9.3, 6.3, 8.0, 7.5, 10.6, 9.4, 8.20, 13.6, 10.2, 12.30, 13.8, 14.55, 11.10, 14.1, 14.45, 11.90, 14.7, 9.10, 13.0)
+)
+
+p.C <- ggplot(df.C, aes(x = x, y = y)) +
+  
+  scale_y_continuous(limits = c(5, 16), breaks = c(5, 10, 15)) +
+  scale_x_continuous(limits = c(5, 16), breaks = c(5, 10, 15)) +
+  
+  theme_classic() +
+  
+  geom_segment(
+    aes(x = 5.1, y = 5.1, xend = 13.4, yend = 13.4),
+    inherit.aes = FALSE,
+    colour = "red3", linewidth = 1.5,
+    arrow = arrow(length = unit(2.1, "mm"), ends = "both", type = "closed")) +
+  
+  geom_line(data = curve_pts, aes(x = x, y = y), color = "orchid2", size = 1.5, inherit.aes = FALSE) +  # Pareto front
+  
+  geom_polygon(data = poly.crv, aes(x, y),
+               fill = "grey60", alpha = 0.3, colour = NA,
+               inherit.aes = FALSE) +
+  
+  geom_point(size= 3) +
+  
+  labs(x = "Trait 1 (e.g. maximum growth rate)",    
+       y = "Trait 2 (e.g. salt tolerance)", 
+       title = "C — genotypic variation") +  # labels
+  
+  theme(
+    legend.position = "none",  
+    axis.title = element_text(size = 12, face = "bold"),  
+    axis.text = element_text(size = 10, face ="plain"),
+    plot.title = element_text(size = 14, face = "bold", hjust = 0.03)# theme stuff
+  ) +
+  
+  annotate("text", x = 13.8, y = 13.8, label = "Pareto front", size = 5.1, fontface = "bold", colour = "orchid2", angle = -45) +
+  
+  coord_cartesian(xlim = c(5,16), ylim = c(5,16), expand = FALSE) 
+
+p.C
+
+# Create panel C.2 — variation across individuals/ genotypes with a gap, PF extended into new space ----------------
+
+df.C2 <- data.frame(
+  x = c(5.7, 6.2, 7.3, 7.4, 8.5, 7.8, 8.8, 9.1, 7.9, 9.5, 10.8, 10.1, 10.2, 11.2, 11.4, 11.3, 12.0, 13.9, 13.55, 13.1, 10.7, 14.55, 9.1, 12.5, 11.9, 14.45, 13.0, 15.25, 10.3, 12.4, 16.1, 14.1, 16.8, 16.5),
+  y = c(5.8, 8.1, 6.5, 9.3, 6.3, 8.0, 12.0, 7.5, 10.6, 9.4, 8.2, 10.1, 12.9, 11.7, 7.4, 9.7, 13.6, 10.1, 12.3, 13.8, 14.55, 11.1, 14.1, 10.55, 14.45, 11.9, 8.70, 15.10, 17.0, 16.7, 14.2, 15.9, 11.2, 12.3)
+)
+
+# Create a second larger quadratic Bezier function to capture the curved pareto frontier
+
+M.2  <- c(15.3, 15.3)      # point the curve should pass through
+P0.2 <- c(17.1, 9.6)       # right endpoint
+P2.2 <- c(9.6, 17.1)       # top endpoint
+
+P1.2 <- (M.2 - (1 - t0)^2 * P0.2 - t0^2 * P2.2) / (2 * (1 - t0) * t0)  # (10,10)
+
+t <- seq(0, 1, length.out = 80)          # curve from P0 -> P2 (right -> top)
+curve_pts.2 <- data.frame(
+  x = (1 - t)^2 * P0.2[1] + 2 * (1 - t) * t * P1.2[1] + t^2 * P2.2[1],
+  y = (1 - t)^2 * P0.2[2] + 2 * (1 - t) * t * P1.2[2] + t^2 * P2.2[2]
+)
+
+p.C2 <- ggplot(df.C2, aes(x = x, y = y)) +
+  
+  scale_y_continuous(limits = c(5, 19), breaks = c(5, 10, 15)) +
+  scale_x_continuous(limits = c(5, 19), breaks = c(5, 10, 15)) +
+  
+  theme_classic() +
+  
+  geom_segment(
+    aes(x = 5.1, y = 5.1, xend = 15.2, yend = 15.2),
+    inherit.aes = FALSE,
+    colour = "red3", linewidth = 1.5,
+    arrow = arrow(length = unit(2.1, "mm"), ends = "both", type = "closed")) +
+  
+  geom_line(data = curve_pts, aes(x = x, y = y), color = "orchid2", size = 1.5, inherit.aes = FALSE) +  # Pareto front
+  
+  geom_line(data = curve_pts.2, aes(x = x, y = y), color = "orchid2", size = 1.5, inherit.aes = FALSE) +  # 2nd Pareto front
+  
+  geom_polygon(data = poly.crv, aes(x, y),
+               fill = "grey60", alpha = 0.3, colour = NA,
+               inherit.aes = FALSE) +
+  
+  geom_point(size= 3) +
+  
+  labs(x = "Trait 1 (e.g. maximum growth rate)",    
+       y = "Trait 2 (e.g. salt tolerance)", 
+       title = "C — genotypic variation") +  # labels
+  
+  theme(
+    legend.position = "none",  
+    axis.title = element_text(size = 12, face = "bold"),  
+    axis.text = element_text(size = 10, face ="plain"),
+    plot.title = element_text(size = 14, face = "bold", hjust = 0.03)# theme stuff
+  ) +
+  
+  annotate("text", x = 15.8, y = 15.8, label = "Pareto fronts", size = 5.1, fontface = "bold", colour = "orchid2", angle = -45) +
+  
+  coord_cartesian(xlim = c(5,19), ylim = c(5,19), expand = FALSE) 
+
+p.C2
+
+# Create panel D - interspecific variation (broad, positive)---------------------------------------
+
+df.D <- data.frame(
   x = c(1.9, 1.3, 2.8, 4.2, 2.2, 3.1, 7.5, 5.0, 6.0, 7.0, 7.0, 8.0, 9.0, 9.0, 10.0, 11.0, 11.0, 12.0, 13.0, 13.0, 14.0, 15.0, 15.0, 16.0, 17.0, 17.0, 18.0, 19.0, 19.0),
   y = c(1.2, 2.1, 2.1, 1.3, 3.8, 8.1, 4.2, 5.0, 6.0, 7.0, 7.0, 8.0, 9.0, 9.0, 10.0, 11.0, 11.0, 12.0, 13.0, 13.0, 14.0, 15.0, 15.0, 16.0, 17.0, 17.0, 18.0, 19.0, 19.0)
 )
