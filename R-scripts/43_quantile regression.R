aa6f9aeffb81252e5b87462bcb47974201cbf9d5
Jason Laurich <jlaurich@uoguelph.ca>
2025-10-02 17:07:15 -0400

    Updated 43...R
    working through it

--- R-scripts/43_quantile regression.R
+++ R-scripts/43_quantile regression.R
@@ -5,3 +5,13 @@
 # I am going to play around with quantile regression
 
 # Load packages & specify functions -----------------------------------------------------------
+
+library(tidyverse)
+library(cowplot)
+library(quantreg)
+
+# Load & examine the data -------------------------------------------------
+
+df <- read.csv("data-processed/20_summary_table.csv") # Summary file
+str(df)
+head(df)
