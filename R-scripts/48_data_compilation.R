53994c650a495a72f1d7029280bd57917a45c902
Jason Laurich <jlaurich@uoguelph.ca>
2025-10-07 12:45:02 -0400

    Added 48...R and 52...csv
    Added the cell size data from Joey's experiment and a new script for generating a massive summary data file.

---
+++ R-scripts/48_data_compilation.R
@@ -0,0 +1,16 @@
+# Jason R Laurich
+
+# October 7th, 2025
+
+# We are going to combine all of the data we need at the individual level to generate a massive summary data frame.
+# I'm also going to explore collapsing variation in pigments into a single axis using PCA.
+
+# Packages & functions ----------------------------------------------------
+
+library(tidyverse)
+library(Deriv)
+library(vegan)  # For PCA
+
+# Load & examine the data -------------------------------------------------
+
+
